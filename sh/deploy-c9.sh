e68dbe652e2aedeab8ad0c15cdef6f175af2388e
Stephane Charoy <trektours.pm@gmail.com>
2016-06-20 11:39:41 +0000

    update deploy

--- sh/deploy-c9.sh
+++ sh/deploy-c9.sh
@@ -3,9 +3,9 @@ mkdir ../deploy
 cd ../deploy
 git clone https://github.com/Puzzlout/CloudDeploy.git
 chmod +x ~/deploy/CloudDeploy/deploy.sh
-chmod +x ~/deploy/CloudDeploy/Projects/TrekTours/install.sh
-chmod +x ~/deploy/CloudDeploy/Projects/TrekTours/refresh.sh
+chmod +x ~/deploy/CloudDeploy/Projects/TrekTours/Api/install.sh
+chmod +x ~/deploy/CloudDeploy/Projects/TrekTours/Api/refresh.sh
 sh ~/deploy/CloudDeploy/deploy.sh
 cd ../workspace
-sh ~/deploy/CloudDeploy/Projects/TrekTours/install.sh $1
-sh ~/deploy/CloudDeploy/Projects/TrekTours/refresh.sh $1
+sh ~/deploy/CloudDeploy/Projects/TrekTours/Api/install.sh $1
+sh ~/deploy/CloudDeploy/Projects/TrekTours/Api/refresh.sh $1
