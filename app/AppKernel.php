d0c920d77b0899b633be4a5556fa211c69edaa7e
Damir <damirius@gmail.com>
2016-06-23 20:02:02 +0200

    #6 Implemented JWT authentication bundle, rewrote tests to use login token, added new test for testing login, setup security firewalls, created ssl keys

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- tests/ApiBundle/Controller/DefaultControllerTest.php
+++ tests/ApiBundle/Controller/DefaultControllerTest.php
@@ -22,4 +22,32 @@ class DefaultControllerTest extends WebTestCase
         $client->request('POST','/');
         $this->assertEquals('405', $client->getResponse()->getStatusCode(), 'Assert that code is 405');
     }
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+
+
+        $client->request('GET', $client->getContainer()->get('router')->generate('api_get_customerinforequests'));
+
+        $this->assertEquals('401', $client->getResponse()->getStatusCode(),
+            'Assert that user can\'t access protected page');
+
+        $client->request('POST', '/login', [
+            '_username' => 'test',
+            '_password' => 'test'
+        ]);
+
+        $jwtToken = json_decode($client->getResponse()->getContent());
+        $jwtToken = $jwtToken->token;
+
+        $client->request('GET', $client->getContainer()->get('router')->generate('api_get_customerinforequests'),
+            [],
+            [],
+            ['HTTP_Authorization' => 'Bearer '.$jwtToken]
+            );
+
+        $this->assertEquals('200', $client->getResponse()->getStatusCode(),
+            'Assert that user can access protected page  after login');
+    }
 }
